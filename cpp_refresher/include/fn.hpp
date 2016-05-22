4062aade399842c597131ef4f4632b1d5334ac5c
madhukarbhat <madhukarbhat@gmail.com>
2016-05-22 14:28:05 +0530

    May 2016 progress

--- cpp_refresher/include/fn.hpp
+++ cpp_refresher/include/fn.hpp
@@ -1,6 +1,7 @@
 #ifndef __FN_HPP_DEFINED
 #define __FN_HPP_DEFINED
 
+void show_params (int& a, int& b, int& c);
 void duplicate (int& a, int& b, int& c);
 
 #endif /*__FN_HPP_DEFINED*/
---
+++ cpp_refresher/include/main.hpp
@@ -0,0 +1,20 @@
+#ifndef MAIN_HPP_DEFINED
+#define MAIN_HPP_DEFINED
+
+#define FILLER_SPACE "  "
+#define CODE_EXIT 0x0
+#define CODE_FN   0x1
+#define CODE_BLAH 0x2
+
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "fn.hpp"
+
+int show_menu_return_choice ();
+void call_fn ();
+
+
+#endif /*MAIN_HPP_DEFINED*/
