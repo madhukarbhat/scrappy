af899f3b9af254ee4e318bdde09dd6fc4ca32914
madhukarbhat <madhukarbhat@gmail.com>
2016-05-22 14:22:26 +0530

    May 2016 progress

--- cpp_refresher/src/fn.cpp
+++ cpp_refresher/src/fn.cpp
@@ -1,8 +1,21 @@
+#include <iostream>
 #include "fn.hpp"
+
+void show_params (int& a, int& b, int& c)
+{
+  std::cout << "---------" << std::endl
+	    << "x = " << a << std::endl
+	    << "y = " << b << std::endl
+	    << "z = " << c << std::endl;
+  return;
+}
+
 
 void duplicate (int& a, int& b, int& c)
 {
-  a*=2;
-  b*=2;
-  c*=2;
+  const int multiplier = 2;
+  a *= multiplier;
+  b *= multiplier;
+  c *= multiplier;
+  return;
 }
--- cpp_refresher/src/main.cpp
+++ cpp_refresher/src/main.cpp
@@ -2,16 +2,78 @@
 //
 // Example: Passing parameters by reference
 
-#include <iostream>
-#include "fn.hpp"
+#include "main.hpp"
 
-int main ()
+// ------------------------------------------------------------------------
+
+int show_menu_return_choice ()
 {
-  int x=1, y=3, z=7;
+  int choice = 0;
+
+  std::cout << "Available options:" << std::endl
+	    << FILLER_SPACE << CODE_EXIT << ". Exit" << std::endl
+	    << FILLER_SPACE << CODE_FN   << ". Execute fn()" << std::endl
+	    << FILLER_SPACE << CODE_BLAH << ". Execute blah ()" << std::endl
+	    << "Enter choice: ";
+
+  std::string usr_input;
+  getline (std::cin,usr_input);
+  std::stringstream (usr_input) >> choice;
+  return choice;
+}
+
+// ------------------------------------------------------------------------
+
+void call_fn ()
+{
+  int x = 1, y = 3, z = 7;
+  std::cout << "Initial Values:" << std::endl;
+  show_params (x, y, z);
+
   duplicate (x, y, z);
-  std::cout << "x = " << x << ", "
-	    << "y = " << y << ", "
-	    << "z = " << z
-	    << std::endl;
+
+  std::cout << "After function call:" << std::endl;
+  show_params (x, y, z);
+  return;
+}
+
+// ------------------------------------------------------------------------
+
+int main (int argc, char* argv[])
+{
+  int command = 99;
+  if (argc == 2) {
+    std::string input (argv[1]);
+    std::stringstream(input) >> command;
+  } else {
+    command = show_menu_return_choice();
+  }
+
+  for (;;) {
+    std::cout << "--------------------------------------------------"
+	      << std::endl;
+    switch (command) {
+    case CODE_EXIT:
+      std::cout << "Bye!" << std::endl;
+      break;
+    case CODE_FN:
+      call_fn();
+      break;
+    case CODE_BLAH:
+      std::cout << command << " : command not implemented, sorry!"
+		<< std::endl;
+      break;
+    default:
+      std::cout << command << " : Not a recognized option, try again."
+		<< std::endl;
+    }
+    if (command != CODE_EXIT) {
+      command = show_menu_return_choice();
+    } else {
+      break;
+    }
+  }
   return 0;
 }
+
+// ------------------------------------------------------------------------
